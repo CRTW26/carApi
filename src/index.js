7f70a5273db1b165b8ae38cf97443d9743f542da
CRTW26 <worthington.connor@gmail.com>
2021-03-27 11:53:20 +0000

    GET /car/:id returns correct car

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
-import cars from '../cars.json'
+import carList from '../cars.json'
 const api = express();
 
 api.get('/cars', (req, res) => {
-  res.send(cars);
+  res.send(carList);
+});
+
+api.get('/car/:id', (req, res) => {
+  let idToFind = parseInt(req.params.id)
+  let carFound;
+  for (let i = 0; i < carList.cars.length; i++) {
+    if (carList.cars[i].id === idToFind) {
+      carFound = carList.cars[i]
+    }
+  }
+  res.send(carFound);
 });
 
 api.listen(3000, () => {
--- tests/index.test.js
+++ tests/index.test.js
@@ -10,4 +10,15 @@ describe('api', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.cars.length).toEqual(1);
   });
+
+  test('GET /car/:id', async () => {
+    const response = await request.get('/car/1');
+    expect(response.statusCode).toBe(200);
+    console.log(response.body)
+    expect(response.body.id).toBe(1);
+    expect(response.body.make).toBe("ford");
+    expect(response.body.model).toBe("fiesta");
+    expect(response.body.buildDate).toBe("02/05/2021");
+    expect(response.body.colourID).toBe(2);
+  });
 })
